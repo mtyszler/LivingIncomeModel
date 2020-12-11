a61b22786a6a2773dd81b18af0cda83ce1026abe
mtyszler <tyszler.jobs@gmail.com>
2020-12-11 15:15:22 +0100

    Improve visuals.py to make function feature_plot flexible on the number of features

--- visuals.py
+++ visuals.py
@@ -116,22 +116,22 @@ def evaluate(results, accuracy):
     pl.show()
     
 
-def feature_plot(importances, X_train, y_train):
+def feature_plot(importances, X_train, y_train, n_features = 5):
     
     # Display the five most important features
     indices = np.argsort(importances)[::-1]
-    columns = X_train.columns.values[indices[:5]]
-    values = importances[indices][:5]
+    columns = X_train.columns.values[indices[:n_features]]
+    values = importances[indices][:n_features]
 
     # Creat the plot
     fig = pl.figure(figsize = (10,15))
-    pl.title("Normalized Weights for First Five Most Predictive Features", fontsize = 16)
-    pl.bar(np.arange(5), values, width = 0.6, align="center", color = '#00A000', \
+    pl.title("Normalized Weights for the " + str(n_features) + " Most Predictive Features", fontsize = 16)
+    pl.bar(np.arange(n_features), values, width = 0.6, align="center", color = '#00A000', \
           label = "Feature Weight")
-    pl.bar(np.arange(5) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
+    pl.bar(np.arange(n_features) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
           label = "Cumulative Feature Weight")
-    pl.xticks(np.arange(5), columns, rotation = 'vertical')
-    pl.xlim((-0.5, 4.5))
+    pl.xticks(np.arange(n_features), columns, rotation = 'vertical')
+    pl.xlim((-0.5, n_features-0.5))
     pl.ylabel("Weight", fontsize = 12)
     pl.xlabel("Feature", fontsize = 12)
     
